3fdbb6cc7a980b9f654be2e5e6c1c9203b75389b
Timothy J McGuire <tmcguire@mcode.com>
2022-10-18 06:32:11 -0500

    Restore working MongoDB Path

--- backend/src/dal/dal.js
+++ backend/src/dal/dal.js
@@ -102,8 +102,7 @@ const logSource = path.basename(__filename);
 // for running in a Docker Container that's running a HOST named "mongo"
 // NOTE: This "mongo" is *not* the Docker Container Name!
 //                  mondodb://hostname:port/appname
-// const DB_URL = `mongodb://${process.env.APP_NAME}-database:${process.env.APP_DATABASE_PORT}`; -- works in DEV
-const DB_URL = `mongodb://localhost:${process.env.APP_DATABASE_PORT}`;
+const DB_URL = `mongodb://${process.env.APP_NAME}-database:${process.env.APP_DATABASE_PORT}/${process.env.APP_NAME}`;
 const DB_NAME = `${process.env.APP_DATABASE_NAME}`;
 
 const BB_OVERDRAFT_FEE = 35.00; // really 'Bad Bank'
@@ -157,7 +156,7 @@ MongoClient.connect(DB_URL, {useUnifiedTopology: true}, (err, client) =>
     {
         db = client.db(DB_NAME);
 
-        mcode.log(`DAL: init - Connected to Mongo DB Server, using database: ${db.databaseName}`, logSource, `info`);
+        mcode.log(`DAL: init - Connected to Mongo DB Server, using database: ${db.databaseName}`, logSource, `success`);
     }
     catch (exception)
     {
